39e31c41e4f08a0a720a81f765c0a96bc5626d6f
prz23 <panruizheng@zju.edu.cn>
2024-07-11 15:36:55 +0800

    update pallets-api

--- src/reg.rs
+++ src/reg.rs
@@ -1,17 +1,6 @@
-use node_visitor::pallets_api::{
-    device_votes_for_current_epoch, device_votes_for_next_epoch, hash_to_version,
-    update_device_version, version_list, BoolSubClient,
-};
-use node_visitor::{
-    node_proxy,
-    pallets_api::{
-        self,
-        bool::runtime_types::{
-            pallet_facility::pallet::DIdentity, pallet_mining::pallet::DeviceState,
-        },
-    },
-};
 use pallets_api::client::SubClient;
+use pallets_api::version_list;
+use pallets_api::{self, bool::runtime_types::pallet_facility::pallet::DIdentity};
 use ringvrf::ed25519::{Keypair, Public, Secret, Signature};
 
 use crate::sgx_key::{get_did, get_secret_key_dcap};
@@ -80,86 +69,29 @@ pub async fn register_sgx_2(
     log::info!("device owner: {}", config_owner);
 
     // Check whether the device has been registered or registered by others
-    let did = get_did(current_version).await;
+    let did = get_did(config_version).await;
     let device_id = DIdentity {
         version: did.0,
         pk: did.1,
     };
-    let device = pallets_api::device_info_v2(&sub_client, device_id.pk.clone(), None).await;
+    let device = pallets_api::device_info_rpc(&sub_client, device_id.pk.clone(), None).await;
     // need register: owner != sender && owner's lock is zero && device's state == standby
-    if let Some(dv) = device {
-        let mut update_version = dv.version;
-        let pk = "0x".to_string() + &hex::encode(&device_id.pk);
-        if dv.state == DeviceState::Working || dv.state == DeviceState::Exiting {
-            if !verify_enclave_hash(&sub_client, dv.version, enclave_hash.clone()).await? {
-                return Err(
-                    format!("Verify working device's enclave hash failed: {enclave_hash:?}"),
-                );
-            }
-        }
-        if "0x".to_string() + &hex::encode(dv.owner.0).to_lowercase() == config_owner.to_lowercase()
-        {
-            if dv.state == DeviceState::Standby && dv.version != current_version {
-                // update version
-                if !verify_enclave_hash(&sub_client, current_version, enclave_hash.clone()).await? {
-                    return Err(
-                        format!("update version failed due to invalid enclave_hash: {enclave_hash:?}"),
-                    );
-                }
-                log::info!(target: "key_server", "device: {:?} will update version to bool", pk);
-                update_version = current_version;
-                update_device_version(
-                    &sub_client,
-                    report.clone(),
-                    current_version,
-                    signature.clone(),
-                )
-                .await
-                .map_err(|_| "update_device_version failed".to_string())?;
-            } else if dv.state == DeviceState::Standby {
-                if !verify_enclave_hash(&sub_client, dv.version, enclave_hash.clone()).await? {
-                    return Err("verify_enclave_hash failed".to_string());
-                }
-            }
-            log::info!(target: "key_server", "device: {:?} has been registered for account: {:?}", pk, config_owner);
-            return Ok(update_version);
-        }
-
-        if let Some(current_votes) =
-            device_votes_for_current_epoch(&sub_client, device_id.pk.clone(), None).await
-        {
-            if let Some(owner_vote) = current_votes.iter().find(|v| v.0 == dv.owner) {
-                if owner_vote.1 != 0 {
-                    return Err(
-                        format!("[{pk:?}] device owner {0:?} has current vote {1:?}", dv.owner, owner_vote.1),
-                    );
-                }
-            }
-        }
-
-        if let Some(current_votes) =
-            device_votes_for_next_epoch(&sub_client, device_id.pk.clone(), None).await
-        {
-            if let Some(owner_vote) = current_votes.iter().find(|v| v.0 == dv.owner) {
-                if owner_vote.1 != 0 {
-                    return Err(
-                        format!("[{pk:?}] device owner {0:?} has current vote {1:?}", dv.owner, owner_vote.1),
-                    );
-                }
-            }
-        }
+    if let Some(_) = device {
+        return Err("registered".to_string());
     }
     // try to register device
-    if !verify_enclave_hash(&sub_client, current_version, enclave_hash.clone()).await? {
+    if !crate::utils::verify_enclave_hash(&sub_client, current_version, enclave_hash.clone())
+        .await?
+    {
         return Err(
-            format!("register device failed due to invalid enclave_hash: {enclave_hash:?}"),
+            "register device failed due to invalid enclave_hash: {:?enclave_hash}".to_string(),
         );
     }
     tokio::spawn(async move {
         loop {
-            let mut did = get_did(current_version).await;
+            let mut did = get_did(config_version).await;
             did.0 = current_version;
-            match node_proxy::call_register_v2(
+            match crate::utils::call_register_rpc(
                 &sub_client,
                 &config_owner,
                 did,
@@ -180,32 +112,12 @@ pub async fn register_sgx_2(
     Ok(current_version)
 }
 
-pub async fn verify_enclave_hash(
-    sub_client: &BoolSubClient,
-    version: u16,
-    enclave_hash: Vec<u8>,
-) -> Result<bool, String> {
-    if version == 0 {
-        return Ok(true);
-    }
-    let online_enclave_list = hash_to_version(sub_client, version, None)
-        .await
-        .ok_or("hash_to_version failed".to_string())?;
-
-    let online_enclave_hashs: Vec<Vec<u8>> = online_enclave_list
-        .as_slice()
-        .chunks(32)
-        .map(|c| c.to_vec())
-        .collect();
-    Ok(online_enclave_hashs.contains(&enclave_hash))
-}
-
 pub fn sign_with_device_sgx_key(msg: Vec<u8>) -> Result<Vec<u8>, String> {
     let key_pair = Keypair::from_secret(ONLINESK.read().unwrap().as_ref().unwrap());
     let msg = sha3_hash256(&msg);
     let sig = key_pair
         .sign(&msg)
-        .map_err(|e| format!("sign error {e:?}"))?
+        .map_err(|_| "sign error".to_string())?
         .as_bytes()
         .to_vec();
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,61 @@
+use pallets_api::hash_to_version;
+use pallets_api::BoolSubClient;
 use sha3::{Digest, Sha3_256};
 
 pub fn sha3_hash256(msg: &[u8]) -> Vec<u8> {
     let mut hasher = Sha3_256::new();
     hasher.input(msg);
     hasher.result()[..].to_vec()
+}
+
+pub fn no_prefix<T: AsRef<str>>(data: T) -> String {
+    data.as_ref()
+        .strip_prefix("0x")
+        .unwrap_or(data.as_ref())
+        .to_string()
+}
+
+pub async fn verify_enclave_hash(
+    sub_client: &BoolSubClient,
+    version: u16,
+    enclave_hash: Vec<u8>,
+) -> Result<bool, String> {
+    if version == 0 {
+        return Ok(true);
+    }
+    let online_enclave_list = hash_to_version(sub_client, version, None)
+        .await
+        .ok_or("hash_to_version failed".to_string())?;
+
+    let online_enclave_hashs: Vec<Vec<u8>> = online_enclave_list
+        .as_slice()
+        .chunks(32)
+        .map(|c| c.to_vec())
+        .collect();
+    Ok(online_enclave_hashs.contains(&enclave_hash))
+}
+
+pub async fn call_register_rpc(
+    sub_client: &BoolSubClient,
+    config_owner: &str,
+    did: (u16, Vec<u8>),
+    report: Vec<u8>,
+    signature: Vec<u8>,
+) -> Result<String, String> {
+    let (version, _pk) = did;
+    let owner = hex::decode(no_prefix(config_owner)).map_err(|e| e.to_string())?;
+    let mut owner_bytes = [0u8; 20];
+    owner_bytes.copy_from_slice(&owner);
+    match pallets_api::register_device_rpc(
+        sub_client,
+        pallets_api::bool::runtime_types::node_primitives::AccountId20(owner_bytes),
+        report,
+        version,
+        signature,
+    )
+    .await
+    {
+        Ok(hash) => Ok("0x".to_string() + &hex::encode(hash.0)),
+        Err(e) => Err(e),
+    }
 }
