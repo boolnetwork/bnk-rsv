bb280f5dcdd825a30587009218f32c54be577e6d
prz23 <panruizheng@zju.edu.cn>
2024-09-04 17:32:25 +0800

    register_sgx_2_not_fetch

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,7 @@ lazy_static! {
 pub use mock::{register_sgx_test, sign_with_device_sgx_key_test, verify_sig_test};
 pub use reg::{
     fetch_relate_device_id, register_sgx_2, sign_with_device_sgx_key, verify_sig,
-    verify_sig_from_string_public,
+    verify_sig_from_string_public, register_sgx_2_not_fetch
 };
 pub use resp_verify::{
     create_sgx_response, create_sgx_response_v2, create_sgx_response_v2_string, sgx_result_parse,
--- src/reg.rs
+++ src/reg.rs
@@ -156,6 +156,122 @@ pub async fn register_sgx_2(
     Ok(current_version)
 }
 
+pub async fn register_sgx_2_not_fetch(
+    subclient_url: String,
+    subclient_warn_time: u128,
+    config_version: u16,
+    device_owner: String,
+    watcher_device_id: String,
+    reg_type: u16,
+) -> Result<u16, String> {
+    let subclient =
+        SubClient::new_from_ecdsa_sk(subclient_url.to_string(), None, Some(subclient_warn_time))
+            .await?;
+
+    let secret_key = get_signer_puls_enclave_key()
+        .await
+        .map_err(|e| e.to_string())?;
+    let secret_key = reg_key(secret_key, reg_type);
+    let public_key: Public = secret_key.into();
+    let key_pair = Keypair::from_secret(&secret_key);
+    let pk_vec = public_key.as_bytes();
+
+    *ONLINESK.write().unwrap() = Some(secret_key);
+    #[cfg(feature = "occlum-enclave")]
+    let (report, enclave_hash) = {
+        use occlum_ra::attestation::{AttestationReport, AttestationStyle, DcapAttestation};
+        use occlum_ra::dcap::DcapAttestationReport;
+
+        let (report_payload, enclave_hash) = {
+            let dcap_report = DcapAttestation::create_report(&pk_vec)
+                .map_err(|_| "Dcap create_report failed".to_string())?;
+            let dar = DcapAttestationReport::from_bytes(dcap_report.quote.clone())
+                .map_err(|_| "Dcap from_bytes failed".to_string())?;
+            let enclave: Vec<u8> = dar.quote.inner.report_body.mr_enclave.m.to_vec();
+            (dcap_report.into_payload(), enclave)
+        };
+        (
+            AttestationReport {
+                style: AttestationStyle::DCAP,
+                data: report_payload,
+            }
+            .into_payload(),
+            enclave_hash,
+        )
+    };
+    #[cfg(not(feature = "occlum-enclave"))]
+    let (report, enclave_hash) = (pk_vec.clone(), vec![0u8; 32]);
+    println!(
+        "enclave hash: 0x{}, attestation: 0x{}",
+        hex::encode(&enclave_hash),
+        hex::encode(&report)
+    );
+
+    let sub_client = subclient.clone();
+    let current_version = version_list(&sub_client, None)
+        .await
+        .ok_or("get version list".to_string())?
+        .pop()
+        .ok_or("version list empty".to_string())?;
+    let msg = [report.clone(), current_version.to_be_bytes().to_vec()].concat();
+    let signature = key_pair
+        .sign(&msg)
+        .map_err(|e| format!("ed25519 sign {e:?}"))?
+        .as_bytes()
+        .to_vec();
+    let config_owner = device_owner.clone();
+    println!("device owner: {}", config_owner);
+    println!("device id pk: {}", hex::encode(&pk_vec));
+
+    // Check whether the device has been registered or registered by others
+    let did = get_did(config_version).await;
+    let device_id = DIdentity {
+        version: did.0,
+        pk: did.1,
+    };
+    let device = pallets_api::device_info_rpc(&sub_client, device_id.pk.clone(), None).await;
+    if let Some(_) = device {
+        println!("registered");
+        return Err("registered".to_string());
+    }
+    // try to register device
+    if !crate::utils::verify_enclave_hash(&sub_client, current_version, enclave_hash.clone())
+        .await?
+    {
+        println!("hash mot match");
+        return Err(
+            "register device failed due to invalid enclave_hash: {:?enclave_hash}".to_string(),
+        );
+    }
+
+    tokio::spawn(async move {
+        loop {
+            let mut did = get_did(config_version).await;
+            did.0 = current_version;
+            match crate::utils::call_register_rpc(
+                &sub_client.clone(),
+                &config_owner,
+                did,
+                report.clone(),
+                signature.clone(),
+                &watcher_device_id,
+            )
+            .await
+            {
+                Ok(res) => {
+                    println!("register sgx: {:?}", res);
+                    
+                    return;
+                }
+                Err(e) => println!("register failed for {:?}", e),
+            }
+            tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+        }
+    });
+
+    Ok(current_version)
+}
+
 pub async fn fetch_relate_device_id(watcher_device_id: Vec<u8>, subclient_url: String) {
     let subclient = SubClient::new_from_ecdsa_sk(subclient_url.to_string(), None, Some(30))
         .await
