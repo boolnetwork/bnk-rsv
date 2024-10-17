6550bd73965e472e7dd9b96639e3d12a44e1317f
prz23 <panruizheng@zju.edu.cn>
2024-10-17 10:05:13 +0800

    remove occlum-ra submodule

--- extern/occlum-ra/start.sh
+++
@@ -1,6 +0,0 @@
-
-mkdir -p /var/run/aesmd && echo '/opt/occlum/start_aesm.sh' >> /root/.bashrc
-LD_LIBRARY_PATH=/opt/intel/sgx-aesm-service/aesm /opt/intel/sgx-aesm-service/aesm/aesm_service &
-
-cd occlum_instance
-occlum run /bin/rust_app
